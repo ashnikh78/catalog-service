7bf0079333f085e2705671860dc01d935e4d3e08
ashnikh78 <ashnikh78@gmail.com>
2025-07-07 18:00:39 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,48 +1,13 @@
-// jest.config.js
-
-const path = require('path');
-
 module.exports = {
-  // Use Node.js environment
   testEnvironment: 'node',
-
-  // Match all .test.js or .spec.js files inside tests/ or src/ directories
-  testMatch: ['**/tests/**/*.(test|spec).js', '**/src/**/*.(test|spec).js'],
-
-  // Automatically clear mocks between every test
-  clearMocks: true,
-
-  // Enable JUnit reporting for Jenkins
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'test-results',
-        outputName: 'junit.xml',
-      },
-    ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js',
+    '!src/config/*.js'
   ],
-
-  // Resolve modules from these directories
-  moduleDirectories: ['node_modules', path.join(__dirname, 'src')],
-
-  // Aliases for cleaner import paths (optional, only if used in code)
-  moduleNameMapper: {
-    '^@controllers(.*)$': '<rootDir>/src/controllers$1',
-    '^@models(.*)$': '<rootDir>/src/models$1',
-    '^@services(.*)$': '<rootDir>/src/services$1',
-    '^@utils(.*)$': '<rootDir>/src/utils$1',
-  },
-
-  // Optional setup file after env is set up (only if `jest.setup.js` exists)
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
-  // Babel transformer (if using ES Modules or JSX)
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-
-  // Verbose output for better test feedback
-  verbose: true,
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/tests/**/*.spec.js'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
 };
